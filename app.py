c3b576f9014f3460b68fb096f2bca28cc417cb66
Christian Classen <christian@cclassen.com>
2022-03-22 22:00:09 -0500

    Finished CleanTweets()

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import datetime
 from datetime import timedelta
 from datetime import timezone
 import tweepy
+import pandas
+import re
 
 
 
@@ -32,7 +34,28 @@ def SearchRecentTweets(query, duration=None):
     
     return result
 
+def SearchTimePeriodTweets(query, start_time, end_time):
+    tweets = client.search_recent_tweets(query=query, start_time=start_time, end_time=end_time, max_results=10)
+    
+    result = []
+    if (tweets.data != None and len(tweets) != 0):
+        for tweet in tweets.data:
+            result.append((tweet.id, tweet.text))
+    
+    return result
 
+def CleanTweets(tweets):
+    for tweet in tweets:
+        tweet["text"] = re.sub(" +", " ", re.sub("#[A-Za-z0-9_]+", "", re.sub("@[A-Za-z0-9_]+", "", tweet["text"])))
+    return tweets
 
-print(SearchRecentTweets("Tesla", timedelta(days=2))[1]["text"])
-print(SearchRecentTweets("Tesla OR Elon Musk OR TSLA", timedelta(days=2))[1]["text"])
+#print(SearchRecentTweets("Tesla", timedelta(days=2))[1][1])
+#print(SearchRecentTweets("Tesla OR Elon Musk OR TSLA", timedelta(days=2))[1][1])
+#print(SearchRecentTweets("#Tesla", timedelta(days=2))[1][1])
+#print(SearchRecentTweets("#Tesla OR #ElonMusk", datetime.datetime(day=1, month=3, year=2022), datetime.datetime(day=1, month=3, year=2022))
+#print(SearchTimePeriodTweets("#Tesla OR #ElonMusk", datetime.datetime(day=1, month=3, year=2022), datetime.datetime(day=1, month=3, year=2022)))
+data = SearchRecentTweets("#Tesla", timedelta(days=2))
+data = CleanTweets(data)
+df = pandas.DataFrame.from_records(data)
+
+print(df)
